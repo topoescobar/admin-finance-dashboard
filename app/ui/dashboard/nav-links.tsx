36d6b8f97d62db757e600d1d8cc37ef88ff41ff1
topoescobar <ivan90ge@gmail.com>
2024-03-30 20:20:04 -0300

    ch 5 refactor

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -18,6 +18,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname()
+  const inactiveStyle = 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'
 
   return (
     <>
@@ -27,11 +28,8 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              {
-                'bg-sky-200 text-blue-600': pathname === link.href,
-              },
+            className={clsx(inactiveStyle,
+              {'bg-sky-200 text-blue-600': pathname === link.href},
             )}
           >
             <LinkIcon className="w-6" />
