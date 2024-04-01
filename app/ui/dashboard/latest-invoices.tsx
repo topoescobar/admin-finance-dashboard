3fde0b092b7afb8ecd1dc546222d9576d4a7cdb2
topoescobar <ivan90ge@gmail.com>
2024-03-31 23:25:47 -0300

    ch 7 fetching data

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,12 +1,12 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import Image from 'next/image'
+import { lusitana } from '@/app/ui/fonts'
+import { LatestInvoice } from '@/app/lib/definitions'
 export default async function LatestInvoices({
   latestInvoices,
 }: {
-  latestInvoices: LatestInvoice[];
+  latestInvoices: LatestInvoice[]
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
@@ -14,9 +14,8 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -51,14 +50,14 @@ export default async function LatestInvoices({
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
-        </div> */}
-        <div className="flex items-center pb-2 pt-6">
+        </div>
+        <div className="flex items-center pb-2 p */}t-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
       </div>
     </div>
-  );
+  )
 }
