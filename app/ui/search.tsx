81089bc342d632d926e2a7ebfc7daeb56fc9c4a4
topoescobar <ivan90ge@gmail.com>
2024-04-04 23:22:04 -0300

    ch 11.0 search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,22 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useRouter, useSearchParams } from 'next/navigation'
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  function handleSearch(query: string) {
+    const params = new URLSearchParams(searchParams)
+    if (query) {
+      params.set('query', query)
+    } else {
+      params.delete('query')
+    }
+    router.replace(`?${params.toString()}`)
+    console.log(query)
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +25,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e)=> handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()} // the native input will manage its own state
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
