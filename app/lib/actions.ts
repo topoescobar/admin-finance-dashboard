273d3837f0ab86ff02b3ce7c8d669d378cf66e91
topoescobar <ivan90ge@gmail.com>
2024-04-07 17:22:26 -0300

    ch 12.2 mutating create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,18 +1,35 @@
 'use server'
-
+import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import { z } from 'zod'
-
 //todas las funciones bajo 'use server' se ejecutan en el servidor y no son accesibles por el cliente.
 
-const createInvoiceFormSchema = z.object({
-  
+//validacion usando zod
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 })
+//omitimos el id y la fecha que no vienen en el form
+const InvoiceFormSchema = InvoiceSchema.omit({ id: true, date: true })
 export async function createInvoice(formData: FormData) {
+  // const allFormData = Object.fromEntries(formData.entries()) //todos los datos del formulario
   const rawFormData = {
     customerId: formData.get('customerId'), //names de los campos del formulario
     amount: formData.get('amount'),
     status: formData.get('status'),
   }
+  const { customerId, amount, status } = InvoiceFormSchema.parse(rawFormData) //data validadada
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0] //el 2do elemento es la hora
 
-  // const allFormData = Object.fromEntries(formData.entries()) //todos los datos del formulario
+  //subir a DB
+  await sql
+   `INSERT INTO invoices (customer_id, amount, status, date) 
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
+  revalidatePath('/dashboard/invoices') //revalidar para que no use datos de cache
+  redirect('/dashboard/invoices')
 }
