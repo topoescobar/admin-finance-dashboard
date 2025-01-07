624279b54a809761a42b5dea29e00b86178d31fe
topoescobar <ivan90ge@gmail.com>
2025-01-07 18:33:29 -0300

    Head tag y limpieza

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts'
+import Head from 'next/head'
 
 export default function RootLayout({
   children,
@@ -9,6 +10,11 @@ export default function RootLayout({
 
   return (
     <html lang="es" className='dark'>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Fractal Lab Dashboard</title>
+      </Head>
       <body className={`${inter.className} antialiased dark:bg-slate-800 dark:text-white`}>{children}</body>
     </html>
   )
