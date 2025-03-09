c1aa11ccc0a64d718094248b1a8bca7da8b92abf
topoescobar <ivan90ge@gmail.com>
2025-03-09 04:10:38 -0300

    responsive y estilo

--- app/ui/customers/customers-table.tsx
+++ app/ui/customers/customers-table.tsx
@@ -9,21 +9,21 @@ export default async function CustomersTable({ query }:
   { query: string }) {
 
   const usersInvestments = await fetchUsersInvestment(query)
-  const {FCAprice, FCDprice} = await cachedLastPrices()
+  const { FCAprice, FCDprice } = await cachedLastPrices()
 
   return (
 
     <div className="mt-6 flow-root">
-      <div className="overflow-x-auto">
+      <div className="sm:overflow-x-auto">
         <div className="inline-block min-w-full align-middle">
-          <div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0 dark:bg-gray-800">
+          <div className="overflow-hidden rounded-lg bg-gray-50 p-2 sm:p-2 dark:bg-gray-700">
 
             {/* MOBILE */}
-            {/*        <div className="md:hidden">
-              {users?.map((user) => (
+            <div className="md:hidden">
+              {usersInvestments?.map((user) => (
                 <div
                   key={user.id}
-                  className="mb-2 w-full rounded-md bg-white p-4 dark:bg-indigo-200 dark:text-indigo-800"
+                  className="mb-2 w-full rounded-md bg-white p-4 bg-gradient-to-br dark:from-indigo-950 dark:via-slate-900 dark:to-slate-800 dark:text-slate-100"
                 >
                   <div className="flex items-center justify-between border-b pb-4">
                     <div>
@@ -43,28 +43,33 @@ export default async function CustomersTable({ query }:
                       <p className="text-sm text-gray-500 emailContainerStyle" >
                         {user.email}
                       </p>
-                      
+
                     </div>
                   </div>
-                  <div className="flex w-full items-center justify-between border-b py-5">
+                  <div className="flex w-full items-center justify-between py-5">
                     <div className="flex w-1/2 flex-col">
-                      <p className="text-xs">Deposito pendiente</p>
-                      <p className="font-medium">{user.total_pending}</p>
+                      <p className="text-lg font-medium">FCA: Ahorro</p>
+                      <p className="text-sm font-normal">{(user.fca_tokens * FCAprice).toFixed(2)} USD</p>
+                      <p className="text-sm font-normal">
+                       PNL: {(user.fca_tokens * FCAprice - user.fca_deposited_usd).toFixed(2)} USD
+                      </p>
+                      <p className="text-xs font-light">{(user.fca_tokens * 1).toFixed(2)} tokens </p>
                     </div>
                     <div className="flex w-1/2 flex-col">
-                      <p className="text-xs">Tokens</p>
-                      <p className="font-medium">{user.total_tokens}</p>
+                      <p className="text-lg font-medium">FCD: Dinámico</p>
+                      <p className="text-sm font-normal">{(user.fcd_tokens * FCDprice).toFixed(2)} USD</p>
+                      <p className="text-sm font-normal">
+                        PNL: {(user.fcd_tokens * FCDprice - user.fcd_deposited_usd).toFixed(2)} USD
+                      </p>
+                      <p className="text-xs font-light">{(user.fcd_tokens * 1).toFixed(2)} tokens</p>
                     </div>
-                  </div>
-                  <div className="pt-4 text-sm">
-                    <p>{user.total_transactions} Transactions</p>
                   </div>
                 </div>
               ))}
             </div>
- */}
+
             {/* DESKTOP */}
-            <table className="hidden min-w-full text-gray-900 md:table dark:text-white">
+            <table className="hidden min-w-full rounded-md text-gray-900 md:table dark:bg-slate-950 dark:text-slate-100">
               <thead className="rounded-lg text-left text-sm font-normal">
                 <tr>
                   <th scope="col" className="px-4 py-5 font-medium sm:pl-6"> Email </th>
@@ -76,7 +81,7 @@ export default async function CustomersTable({ query }:
                 </tr>
               </thead>
 
-              <tbody className="bg-white dark:bg-slate-700">
+              <tbody className="bg-white bg-gradient-to-br dark:from-slate-900 dark:to-slate-800">
                 {usersInvestments?.map((user) => (
                   <tr key={user.id} className="group w-full border-b py-2 text-sm">
                     <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm sm:pl-6">
